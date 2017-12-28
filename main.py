8d414f7f3a5d187b6c7aa89afaca72907e4e54ea
BlizzArchonJ <gopengy@gmail.com>
2017-12-28 00:18:42 -0500

    reflects zero-width space fix and choice to join nameLists or not

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from xml.etree import ElementTree
 
-def ftl_names_v2(node, names, short_names):
+def sort_names(node, names, short_names):
     """ Sorts the <name /> tags in node into short_names if the tag contains
         a "short" attribute, or names if the tag is just plain.
 
@@ -23,12 +23,13 @@ def ftl_names_v2(node, names, short_names):
 
 def write_names(wf, short_male, reg_male, short_female, reg_female):
     """ Writes the names of two nameLists in "alphabetical" order into wf.
+    Adds zero-width spaces to names in the female nameList to make them unique.
 
     :param wf: a file to write to
-    :param short_male: list with the <name="short"> tags for male nameList
-    :param reg_male: list with the <name> tags for male nameList
+    :param short_male:   list with the <name="short"> tags for male nameList
+    :param reg_male:     list with the <name> tags for male nameList
     :param short_female: list with the <name="short"> tags for female nameList
-    :param reg_female: list with the <name> tags for female nameList
+    :param reg_female:   list with the <name> tags for female nameList
     :rtype: None
     """
     wf.write("<nameList race=\"human\" sex=\"male\">\n")
@@ -41,45 +42,58 @@ def write_names(wf, short_male, reg_male, short_female, reg_female):
     wf.write("</nameList>\n\n"
              "<nameList race=\"human\" sex=\"female\">\n")
     for name_tuple in sorted(short_female):
-        wf.write("\t<name short=\"{}\">{}</name>\n"
+        wf.write("\t<name short=\"{}\">{}\u200b</name>\n"
                  "".format(name_tuple[0], name_tuple[1]))
     for name_string in sorted(reg_female):
-        wf.write("\t<name>{}</name>\n"
+        wf.write("\t<name>{}\u200b</name>\n"
                  "".format(name_string))
     wf.write("</nameList>\n")
 
 
-def xmlize(event_root, writer):
+def xmlize(event_root, writer, join_truth):
     """ Does XML stuff using event_root as a tree representing a read-only file
-        and writer as as a write-only file.
+        and writer as a write-only file.
 
     :param event_root: the head node of a tree containing XML data
     :param writer: a file open for writing
+    :param join_truth: True if all nameLists should be joined,
+                       regardless of gender. 
     :rtype: None
     """
-    naming = False
-    male_names_list, male_short_names_list = [], []
-    female_names_list, female_short_names_list = [], []
-    # above used to be names_list and short_names_list for both genders
-    for child in event_root:
-        if child.tag == 'nameList':
-            if not naming:
-                naming = True
-            if child.attrib['sex'] == 'male':
-                ftl_names_v2(child, male_names_list, male_short_names_list)
-            elif child.attrib['sex'] == 'female':
-                ftl_names_v2(child, female_names_list, female_short_names_list)
-            else:
-                raise ValueError("nameList's 'sex' attribute must "
-                                 "either be male or female!\n")
-    if naming:
-        write_names(writer, male_short_names_list, male_names_list,
-                    female_short_names_list, female_names_list)
+    if join_truth:
+        names_list, short_names_list = [], []
+        for child in event_root:
+            if child.tag == 'nameList':
+                if child.attrib['sex'] == 'male' or child.attrib['sex'] == 'female':
+                    sort_names(child, names_list, short_names_list)
+                else:
+                    raise ValueError("nameList's 'sex' attribute must "
+                                     "either be male or female!\n")
+        # if both empty, don't write
+        if names_list or short_names_list:
+            write_names(writer, short_names_list, names_list,
+                        short_names_list, names_list)
+    else:
+        m_names_list, m_short_names_list = [], []
+        f_names_list, f_short_names_list = [], []
+        for child in event_root:
+            if child.tag == 'nameList':
+                if child.attrib['sex'] == 'male':
+                    sort_names(child, m_names_list, m_short_names_list)
+                elif child.attrib['sex'] == 'female':
+                    sort_names(child, f_names_list, f_short_names_list)
+                else:
+                    raise ValueError("nameList's 'sex' attribute must "
+                                     "either be male or female!\n")
+        # if both male nameList and female nameList empty, don't write
+        if (m_names_list or m_short_names_list) and (f_names_list or f_short_names_list):
+            write_names(writer, m_short_names_list, m_names_list,
+                        f_short_names_list, f_names_list)
 
 
 if __name__ == '__main__':
 
-    reading = input("\nEnter name of file in this directory to read from:\n")
+    reading = input("Enter name of file in this directory to read from:\n")
 
     with open(reading, 'r') as reading_file:
         tree = ElementTree.parse(reading_file)
@@ -87,4 +101,10 @@ if __name__ == '__main__':
 
         writing = input("Enter name of new file name to write to.\n")
         with open(writing, 'w') as writing_file:
-            xmlize(root, writing_file)
+            join_all = input("Join all nameLists regardless of gender? (enter y or n)\n")
+            # default is to keep separate -- entered n
+            join_regardless = False
+            if join_all == 'y':
+                join_regardless = True
+            
+            xmlize(root, writing_file, join_regardless)
